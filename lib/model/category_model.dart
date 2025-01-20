bfa629730531d0dd4123a0c05ebcf7977153b7dc
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2025-01-20 14:48:37 +0530

    category ui + api integration completed

--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -3,14 +3,14 @@ class CategoryModel {
   final String name;
   final String image;
   final bool isEmpty;
-  final String color; // Add color property
+  final String color; 
 
   CategoryModel({
     required this.id,
     required this.name,
     required this.image,
     this.isEmpty = false,
-    this.color = '0xFFE5FFE3', // default color if not available from backend
+    this.color = '0xFFE5FFE3', 
   });
 
   factory CategoryModel.fromJson(Map<String, dynamic> json) {
@@ -18,7 +18,7 @@ class CategoryModel {
       id: json['id'].toString(),
       name: json['name'] ?? '',
       image: json['image'] ?? '',
-      color: json['color'] ?? '0xFFE5FFE3', // Fetch color from the backend
+      color: json['color'] ?? '0xFFE5FFE3', 
     );
   }
 
--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -168,4 +168,3 @@ class CategoryItemWidget extends StatelessWidget {
     );
   }
 }
-
